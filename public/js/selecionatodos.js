807a29d9774b926dca3c8d6578e3351cc9160c6d
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-09-28 18:30:43 -0300

    Criação do arquivo selecionatodos.js que contém as funções que controlam a ação do checkbox marcar todos.

---
+++ public/js/selecionatodos.js
@@ -0,0 +1,139 @@
+/*Função usada para ao marcar o checkbox todos ser marcado todos os check box abaixo dele, ou seja todos os checkbox do respectivo dia da semana
+
+
+
+	@Author: Michel Oliveira - 2017
+*/
+window.onload = function() {
+	document.getElementById("todosSM").onclick = function() {
+	            var tab = document.getElementById("marcartodosM"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "segunda[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosTM").onclick = function() {
+	            var tab = document.getElementById("marcartodosM"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "terca[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosQaM").onclick = function() {
+	            var tab = document.getElementById("marcartodosM"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "quarta[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosQiM").onclick = function() {
+	            var tab = document.getElementById("marcartodosM"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "quinta[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosSeM").onclick = function() {
+	            var tab = document.getElementById("marcartodosM"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "sexta[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosSaM").onclick = function() {
+	            var tab = document.getElementById("marcartodosM"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "sabado[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosSN").onclick = function() {
+	            var tab = document.getElementById("marcartodosN"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "segunda[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosTN").onclick = function() {
+	            var tab = document.getElementById("marcartodosN"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "terca[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosQaN").onclick = function() {
+	            var tab = document.getElementById("marcartodosN"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "quarta[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosQiN").onclick = function() {
+	            var tab = document.getElementById("marcartodosN"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "quinta[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosSeN").onclick = function() {
+	            var tab = document.getElementById("marcartodosN"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "sexta[]")
+	                    chk.checked = this.checked;
+	            }
+	}
+
+	document.getElementById("todosSaN").onclick = function() {
+	            var tab = document.getElementById("marcartodosN"); //formulário
+	            //percorre todos os checkboxes e seta se está ou não checado, conforme o valor do check mandatório
+	            var checks = tab.getElementsByTagName("input");
+	            for(var i=0; i<checks.length; i++) {
+	                var chk = checks[i];
+	                if(chk.name == "sabado[]")
+	                    chk.checked = this.checked;
+	            }
+	}             
+}
