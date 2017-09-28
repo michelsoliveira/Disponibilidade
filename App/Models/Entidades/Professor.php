35a2500a1458ba83ea84c751eb5f07025c6f264c
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-09-28 18:31:30 -0300

    Criado a classe professor do model.

---
+++ App/Models/Entidades/Professor.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Professor
+ *
+ * @author michel.oliveira
+ */
+class Professor {
+    //put your code here
+}
