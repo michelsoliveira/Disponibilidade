8b5590dd286e88b6b45c4aba56d2ad25f645fc7b
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-09-28 14:46:58 -0300

    Criação do layout do header.

---
+++ App/Views/Layouts/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt-br">
+    
+    <head>
+        <title>Cadastro de disponibilidade de horário.</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link href="../../../public/css/bootstrap.min.css" rel="stylesheet">
+                
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+   
+    <body>
