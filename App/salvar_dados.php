939636d922769f0744bc274eb8c1d282d5ae5084
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-10-23 16:54:22 -0200

    criação da função de salvar os dados do usuário que preencheu o formulário com exceção da grade de horários.

---
+++ App/salvar_dados.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    session_start();
+
+    try 
+    {
+        $conn = Conexao::getConexao();
+
+    } 
+    catch (Exception $ex) 
+    {
+
+    }
+
+    function salvarDadosUsuario($nome,$telefone,$email,$discapto,$outrasinfo)    
+    {
+        $sql = "CALL INSERE_PROFESSOR(:nome,:email,:telefone,:discapto,:outrasinfo)";
+        try{
+            
+            $conn = Conexao::getConexao();
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $nome, PDO::PARAM_STR);
+            $stmt->bindValue(':telefone', $nome, PDO::PARAM_STR);
+            $stmt->bindValue(':discapto', $nome, PDO::PARAM_STR);
+            $stmt->bindValue(':outrasinfo', $nome, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+        catch (Exception $e)
+        {
+            throw  new Exception("Erro ao Salvar dados do usuário");
+        }
+    }
+    
+    function SalvarHorários
