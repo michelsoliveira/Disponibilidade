70f733cae5012de8d7114498760a9bf05e392565
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-10-26 14:57:16 -0200

    criada a função base para gerar um xml

---
+++ App/gerarXml.php
@@ -0,0 +1,60 @@
+<?php
+        require_once 'funcoes.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    #versao do encoding xml
+    $dom = new DOMDocument("1.0", "ISO-8859-1");
+
+    #retirar os espacos em branco
+    $dom->preserveWhiteSpace = false;
+
+    #gerar o codigo
+    $dom->formatOutput = true;
+
+    #criando o nó principal (root)
+    $root = $dom->createElement("EXPORT_URANIA_INSERT");
+
+    #nó filho (PROFESSORES)
+    $professores = $dom->createElement("PROFESSORES");
+
+    #nó filho (REGISTRO)
+    $registro = $dom->createElement("REGISTRO");
+
+
+    #setanto nomes e atributos dos elementos xml (nós)
+    $codigo = $dom->createElement("CODIGO", "123");
+    $nome = $dom->createElement("NOME", "Rafael Clares");
+    $segunda = $dom->createElement("SEG", "11111");
+    $terca = $dom->createElement("TER", "000000");
+    $quarta = $dom->createElement("QUA", "111000");
+    $quinta = $dom->createElement("QUI", "111000");
+    $sexta = $dom->createElement("SEX", "000111");
+    $sabado = $dom->createElement("SAB", "101010");
+
+    #adiciona os nós (informacaoes do contato) em contato
+    $registro->appendChild($codigo);
+    $registro->appendChild($nome);
+    $registro->appendChild($segunda);
+    $registro->appendChild($terca);
+    $registro->appendChild($quarta);
+    $registro->appendChild($quinta);
+    $registro->appendChild($sexta);
+    $registro->appendChild($sabado);
+
+    #adiciona o nó contato em (root) agenda
+    $root->appendChild($professores);
+    $professores->appendChild($registro);
+    $dom->appendChild($root);
+
+    # Para salvar o arquivo, descomente a linha
+    $dom->save("arquivos/URANEXP.xml");
+
+    #cabeçalho da página
+    header("Content-Type: text/xml");
+    # imprime o xml na tela
+    //print $dom->saveXML();
+
