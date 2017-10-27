f06dcb1b53c21290ccedb563161e058fd9cff22f
Michel Oliveira <michel.s.oliveira@gmail.com>
2017-10-27 17:08:59 -0200

    criado os loops para preencher o xml do horario da manhã

--- App/gerarXml.php
+++ App/gerarXml.php
@@ -8,8 +8,16 @@
  */
         
     $qtdp = Funcoes::contadorProf();
-    $qtdd = Funcoes::contadorDisp();
-        
+    
+    $db = Database::conexao();
+
+    
+
+
+
+
+
+    
     #versao do encoding xml
     $dom = new DOMDocument("1.0", "ISO-8859-1");
 
@@ -25,23 +33,70 @@
     #nó filho (PROFESSORES)
     $professores = $dom->createElement("PROFESSORES");
 
-    for($i=0; $i<$qtdp; $i++)
+    while($row = $result->fetch(PDO::FETCH_OBJ))
     {
         #nó filho (REGISTRO)
         $registro = $dom->createElement("REGISTRO");
 
 
         #setanto nomes e atributos dos elementos xml (nós)
-        $codigo = $dom->createElement("CODIGO", "123");
-        $nome = $dom->createElement("NOME", "Rafael Clares");
-        $segunda = $dom->createElement("SEG", "11111");
-        $terca = $dom->createElement("TER", "000000");
-        $quarta = $dom->createElement("QUA", "111000");
-        $quinta = $dom->createElement("QUI", "111000");
-        $sexta = $dom->createElement("SEX", "000111");
-        $sabado = $dom->createElement("SAB", "101010");
-
-        #adiciona os nós (informacaoes do contato) em contato
+        $codigo = $dom->createElement("CODIGO", $row->matricula);
+        $nome = $dom->createElement("NOME", $row->nome);
+        
+        while($row2 = $result2->fetch(PDO::FETCH_OBJ))
+        {
+            if($row2->diasemana_id == 1)
+            {
+                $segunda = $dom->createElement("SEG", $row2->horario);
+            }
+            else
+            {
+                $segunda = $dom->createElement("SEG", "00000");
+            }
+            if($row2->diasemana_id == 2)
+            {
+                $terca = $dom->createElement("TER", $row2->horario);
+            }
+            else
+            {
+                $terca = $dom->createElement("TER", "00000");
+            }
+            if($row2->diasemana_id == 3)
+            {
+                $quarta = $dom->createElement("QUA", $row2->horario);
+            }
+            else
+            {
+                $quarta = $dom->createElement("QUA", "00000");
+            }
+            if($row2->diasemana_id == 4)
+            {
+                $quinta = $dom->createElement("QUI", $row2->horario);
+            }
+            else
+            {
+                $quinta = $dom->createElement("QUI", "00000");
+            }
+            if($row2->diasemana_id == 5)
+            {
+                $sexta = $dom->createElement("SEX", $row2->horario);
+            }
+            else
+            {
+                $sexta = $dom->createElement("SEX", "00000");
+            }
+            if($row2->diasemana_id == 6)
+            {
+                $sabado = $dom->createElement("SAB", $row2->horario);
+            }
+            else
+            {
+                $sabado = $dom->createElement("SAB", "00000");
+            }
+        }
+    
+    
+        #adiciona os nós (informacaoes do professor) em registro
         $registro->appendChild($codigo);
         $registro->appendChild($nome);
         $registro->appendChild($segunda);
@@ -50,9 +105,8 @@
         $registro->appendChild($quinta);
         $registro->appendChild($sexta);
         $registro->appendChild($sabado);
-
-        #adiciona o nó contato em (root) agenda
         
+        #adiciona o nó contato em (root) professor
         $professores->appendChild($registro);
      
     }    
@@ -67,4 +121,6 @@
     header("Content-Type: text/xml");
     # imprime o xml na tela
     //print $dom->saveXML();
+    
+    
 
